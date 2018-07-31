afbde77e1bf87cbd90e75b4f7c87e8011bab8f15
unfaiyted <unfaiyted@gmail.com>
2018-07-31 14:47:18 -0500

    adding client server

---
+++ server/src/main/java/com/faiyted/blab/BlabApplication.java
@@ -0,0 +1,12 @@
+package com.faiyted.blab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlabApplication.class, args);
+	}
+}
